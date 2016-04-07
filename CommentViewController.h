3acd9a71f9e17e260dc43889b7e2e66f6bf61817
ravelasquez99 <ricky.wedew@gmail.com>
2016-04-07 16:33:09 -0700

    VC's connected to proper files

---
+++ CommentViewController.h
@@ -0,0 +1,13 @@
+//
+//  CommentViewController.h
+//  InstagramClone
+//
+//  Created by Richard Velazquez on 4/7/16.
+//  Copyright © 2016 Team4. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CommentViewController : UIViewController
+
+@end
---
+++ FinalizeViewController.m
@@ -0,0 +1,37 @@
+//
+//  FinalizeViewController.m
+//  InstagramClone
+//
+//  Created by Richard Velazquez on 4/7/16.
+//  Copyright © 2016 Team4. All rights reserved.
+//
+
+#import "FinalizeViewController.h"
+
+@interface FinalizeViewController ()
+
+@end
+
+@implementation FinalizeViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
