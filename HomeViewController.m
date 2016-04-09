0e97630e23c60bff04375e95c128e34a2b40acf0
ravelasquez99 <ricky.wedew@gmail.com>
2016-04-08 21:28:14 -0700

    Default pictures load from assets or core data depending on what the current user state is

--- HomeViewController.m
+++ HomeViewController.m
@@ -10,9 +10,15 @@
 // made changes 7:08
 
 #import "HomeViewController.h"
+#import "AppDelegate.h"
+#import "Photo.h"
+#import "PhotoCollectionViewCell.h"
 
 @interface HomeViewController ()<UICollectionViewDataSource, UICollectionViewDelegate>
 @property (weak, nonatomic) IBOutlet UISegmentedControl *segmentedControl;
+@property NSMutableArray *photos;
+@property NSManagedObjectContext *moc;
+@property (weak, nonatomic) IBOutlet UICollectionView *collectionView;
 
 @end
 
@@ -28,7 +34,17 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    AppDelegate *appDelegate = [UIApplication sharedApplication].delegate;
+    self.moc = appDelegate.managedObjectContext;
+  
+    [self pullPhotosFromCoreData];
+  
+    if (self.photos.count == 0) {
+      [self createDefaultPhotosAndSaveToCoreData];
+    }
+  
+  [self.collectionView reloadData];
+
 }
 
 - (void)didReceiveMemoryWarning {
@@ -42,15 +58,66 @@
   
 }
 
+#pragma CoreData
+-(void)createDefaultPhotosAndSaveToCoreData{
+  self.photos = [NSMutableArray new];
+  
+  for (int i = 0; i < 5; i++) {
+   
+    NSString *photoString = [NSString stringWithFormat:@"image%i", i];
+    UIImage *newImage = [UIImage imageNamed:photoString];
+    
+    
+    Photo *newPhoto = [NSEntityDescription insertNewObjectForEntityForName:@"Photo" inManagedObjectContext:self.moc];
+    newPhoto.photoImage = UIImageJPEGRepresentation(newImage, 1.0);
+    newPhoto.photoDescription = [NSString stringWithFormat:@"Image:%i description", i];
+    newPhoto.photoTimestamp = nil;
+    
+    [self.photos addObject:newPhoto];
+  }
+  
+  NSError *error;
+  if ([self.moc save:&error]) {
+    NSLog(@"Save was a success");
+    NSLog(@"%lu is how many photos that were saved to core data", (unsigned long)self.photos.count);
+  } else {
+    NSLog(@"failed because %@", error);
+  }
+  
+}
+
+-(void)pullPhotosFromCoreData {
+  
+  self.photos = [NSMutableArray new];
+  NSFetchRequest *request = [[NSFetchRequest alloc]initWithEntityName:@"Photo"];
+  
+  NSError *error;
+  self.photos = [[self.moc executeFetchRequest:request error:&error]mutableCopy];
+  
+  if (error == nil) {
+    NSLog(@"pull was successful");
+  } else {
+    NSLog(@"Error: %@", error);
+  }
+  
+  
+}
+
+
 #pragma Collection View
 -(NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section{
-  return 10;
+  return self.photos.count;
   
 }
 
 -(UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath{
   
-  UICollectionViewCell *cell = [collectionView dequeueReusableCellWithReuseIdentifier:@"homeCell" forIndexPath:indexPath];
+  Photo *tempPhoto = [self.photos objectAtIndex:indexPath.row];
+
+  
+  PhotoCollectionViewCell *cell = [collectionView dequeueReusableCellWithReuseIdentifier:@"homeCell" forIndexPath:indexPath];
+  NSData *imageData = tempPhoto.photoImage;
+  cell.imageView.image = [UIImage imageWithData:imageData];
   
   return cell;
 }
---
+++ InstagramClone/PhotoCollectionViewCell.m
@@ -0,0 +1,13 @@
+//
+//  PhotoCollectionViewCell.m
+//  InstagramClone
+//
+//  Created by Richard Velazquez on 4/8/16.
+//  Copyright © 2016 Team4. All rights reserved.
+//
+
+#import "PhotoCollectionViewCell.h"
+
+@implementation PhotoCollectionViewCell
+
+@end
