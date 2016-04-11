b38aa92efb8a07386c91337c806030a071b31493
James Rochabrun <jamesrochabrun@gmail.com>
2016-04-11 13:40:40 -0700

    first version camera working, needs to fix sided photos

--- AddPhotoViewController.m
+++ AddPhotoViewController.m
@@ -7,8 +7,17 @@
 //
 
 #import "AddPhotoViewController.h"
+#import "AppDelegate.h"
+#import "User.h"
+#import "Comment.h"
+#import "Photo.h"
+#import "FinalizeViewController.h"
 
-@interface AddPhotoViewController ()
+@interface AddPhotoViewController ()<UIImagePickerControllerDelegate, UINavigationControllerDelegate>
+@property (nonatomic, strong) UIImage *pickedImage;
+@property NSManagedObjectContext *moc;
+@property NSMutableArray *photosArray;
+@property (weak, nonatomic) IBOutlet UIImageView *imageView;
 
 @end
 
@@ -16,22 +25,118 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
-}
+    AppDelegate *appDelegate = [UIApplication sharedApplication].delegate;
+    NSLog(@"sqlite dir = \n%@", appDelegate.applicationDocumentsDirectory);
+    
+    self.moc = appDelegate.managedObjectContext;
+    
+    self.photosArray = [NSMutableArray new];}
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
 }
 
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+#pragma Camera
+//this sets the pickedImage
+-(void)imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary<NSString *,id> *)info
+{
+    UIImage *image = info[UIImagePickerControllerOriginalImage];
+    self.pickedImage = image;
+    
+    [self dismissViewControllerAnimated:YES completion:nil];
 }
-*/
+
+//this cancels the action (the cancel in the top right provided by apple)
+-(void)imagePickerControllerDidCancel:(UIImagePickerController *)picker
+{
+    [self dismissViewControllerAnimated:YES completion:nil];
+}
+
+//this sets the pickedImage  in to the view
+-(void)setPickedImage:(UIImage *)pickedImage
+{
+    _pickedImage = pickedImage;
+    
+    //here is where you can set the picked image
+    [self.imageView setImage:pickedImage];
+    
+}
+
+//user selects library
+- (IBAction)onLibraryPressedButton:(UIButton *)sender {
+    UIImagePickerController *controller = [[UIImagePickerController alloc] init];
+    controller.sourceType = UIImagePickerControllerSourceTypePhotoLibrary;
+    controller.delegate = self;
+    [self presentViewController:controller animated:YES completion:nil];
+}
+
+//user selects camara
+- (IBAction)onCameraButtonPressed:(UIButton *)sender {
+    
+    UIImagePickerController *controller = [[UIImagePickerController alloc] init];
+    controller.sourceType = UIImagePickerControllerSourceTypeCamera;
+    controller.delegate = self;
+    [self presentViewController:controller animated:YES completion:nil];
+}
+
+
+- (IBAction)dismissCameraActions:(UIButton *)sender {
+    [self dismissViewControllerAnimated:YES completion:nil];
+}
+
+
+//I passed the NSDATA Photo to the finalize VC
+-(void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    
+    Photo *photo = [NSEntityDescription insertNewObjectForEntityForName:@"Photo" inManagedObjectContext:self.moc];
+    
+    //this is for the attribute value
+    NSData *imageData = UIImagePNGRepresentation(self.pickedImage);
+    
+    photo.photoImage = [NSData dataWithData:imageData];
+    
+    //sending the object Photo to the Finalize
+    FinalizeViewController *destVC = segue.destinationViewController;
+    destVC.photo = photo;
+    
+    //sending a user to stablish a relationship
+    destVC.user = self.user;
+    
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 @end
+
+
+
+
+
+
+
+
+
+
+
+
--- FinalizeViewController.m
+++ FinalizeViewController.m
@@ -7,8 +7,19 @@
 //
 
 #import "FinalizeViewController.h"
+#import "User.h"
+#import "Photo.h"
+#import "Comment.h"
+#import "AppDelegate.h"
+
 
 @interface FinalizeViewController ()
+@property NSManagedObjectContext *moc;
+@property NSMutableArray *photos;
+@property (weak, nonatomic) IBOutlet UIImageView *imageView;
+@property (weak, nonatomic) IBOutlet UITextView *textView;
+
+
 
 @end
 
@@ -16,12 +27,55 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    AppDelegate *appDelegate = [UIApplication sharedApplication].delegate;
+    NSLog(@"sqlite dir = \n%@", appDelegate.applicationDocumentsDirectory);
+    
+    NSLog(@"MY NAME IS %@ and i am checking that i was correctly passed to this view",   self.user.userName);
+    
+    self.moc = appDelegate.managedObjectContext;
+    
+    //displaying the image in the finalize VC
+    NSData *imagedata = self.photo.photoImage;
+    
+    self.imageView.image = [UIImage imageWithData:imagedata];
+    
+    self.photos = [[NSMutableArray alloc]init];
 }
 
+//this action has 2 actions, the exit segue and add the photoin coredata
+
 - (IBAction)onShareButtonPressed:(UIButton *)sender {
-  
-  
+    self.photo.photoDescription = self.textView.text;
+    
+    self.photo.photoTimestamp =  [NSDate date];
+    
+    //setting the relationships
+    //adding the picture to the user
+    [self.user addPhotosObject:self.photo];
+    
+    
+    NSError *error;
+    
+    if ([self.moc save:&error]){
+        [self.photos addObject:self.photo];
+        NSLog(@"%@,  %lu  in the array of photos", self.photo, self.photos.count);
+    }else{
+        NSLog(@"an error has occurred,...%@",error);
+    }
 }
+
+
+
+
+
+#pragma dismiss view
+
+- (IBAction)cancelSharePhoto:(UIButton *)sender {
+    [self dismissViewControllerAnimated:YES completion:nil];
+
+}
+
+
 
 
 
